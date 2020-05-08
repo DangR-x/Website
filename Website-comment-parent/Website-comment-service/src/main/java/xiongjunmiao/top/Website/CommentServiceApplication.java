0b92dbf658d00d0a5f32486a7fab83d592928884
myth_hai@163.com <myth_hai@163.com>
2020-05-08 19:19:14 +0800

    定时任务

--- Website-comment-parent/Website-comment-service/src/main/java/xiongjunmiao/top/Website/CommentServiceApplication.java
+++ Website-comment-parent/Website-comment-service/src/main/java/xiongjunmiao/top/Website/CommentServiceApplication.java
@@ -3,6 +3,8 @@ package xiongjunmiao.top.Website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
@@ -14,10 +16,16 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @SpringBootApplication
 //将mapper注册进spring容器当中
 @MapperScan("xiongjunmiao.top.Website.mapper")
+//@EnableScheduling         //开启定时任务
 public class CommentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommentServiceApplication.class,args);
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
     }
+    //表示方法执行完成后5秒
+//    @Scheduled(fixedDelay = 5000)
+//    public void schedulin(){
+//        System.out.println("定时任务");
+//    }
 }
